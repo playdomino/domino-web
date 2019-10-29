1206a96944ca05b07c731c26c4cd7b0cdd7cdba7
Claudiu GS <claudiugabriel15@yahoo.com>
2019-10-29 22:18:54 +0100

    - set up eslint - set up babylon js - set up a basic 3d scene

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import SceneLoader from './Components/SceneLoader/SceneLoader';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SceneLoader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SceneComponent/SceneComponent.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { 
+	Engine,
+	Scene,
+} from '@babylonjs/core';
+
+class SceneComponent extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+  }
+
+  componentDidMount() {
+    const {
+      engineOptions,
+      adaptToDeviceRatio,
+      onSceneMount,
+    } = this.props;
+
+    this.engine = new Engine(
+      this.canvas,
+      true,
+      engineOptions,
+      adaptToDeviceRatio,
+    );
+
+    const scene = new Scene(this.engine);
+    this.scene = scene;
+
+    if (typeof onSceneMount === 'function') {
+      onSceneMount({
+        scene,
+        engine: this.engine,
+        canvas: this.canvas,
+      });
+    } else {
+      console.error('onSceneMount function not available');
+    }
+
+    // Resize the babylon engine when the window is resized
+    window.addEventListener('resize', this.onResizeWindow);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResizeWindow);
+  }
+
+  onResizeWindow = (e) => {
+    this.setState({ 
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    if (this.engine) {
+      this.engine.resize();
+    }
+  }
+
+  onCanvasLoaded = (c) => {
+    if (c !== null) {
+      this.canvas = c;
+    }
+  }
+
+  render() {
+    // 'rest' can contain additional properties that you can flow through to canvas:
+    // (id, className, etc.)
+    const { width, height } = this.state;
+
+    const opts = {};
+
+    if (width !== undefined && height !== undefined) {
+      opts.width = width;
+      opts.height = height;
+    }
+
+    return (
+      <canvas
+        width={width}
+        height={height}
+        ref={this.onCanvasLoaded}
+      />
+    );
+  }
+}
+
+Scene.propTypes = {
+  someProp: PropTypes.number.isRequired,
+  onTap: PropTypes.func.isRequired,
+  engineOptions: PropTypes.number.isRequired,
+  adaptToDeviceRatio: PropTypes.number.isRequired,
+  onSceneMount: PropTypes.func.isRequired,
+};
+
+export default SceneComponent;
